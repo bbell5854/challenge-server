37a85b5eded1fbb3cd6d4b16b98909e720d700ac
brad <a>
2019-11-16 16:14:25 -0500

    Adding logger level

--- lib/utils/logger.ts
+++ lib/utils/logger.ts
@@ -1,7 +1,9 @@
 import bunyan from "bunyan";
 
+const env = process.env.ENVIRONMENT;
 const logger = bunyan.createLogger({
   name: "challenge-server",
+  level: env === "prd" ? "info" : "debug",
 });
 
 export default logger;
